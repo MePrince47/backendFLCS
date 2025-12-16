26a00aeab2f5a02ff590fc59ead867af07d13518
Kira58500 <fokouapatie@mail.com>
2025-12-16 04:25:58 +0100

    Premier commit

---
+++ GESTION_FLCS/src/main/java/FLCS/GESTION/GestionFlcsApplication.java
@@ -0,0 +1,13 @@
+package FLCS.GESTION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFlcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFlcsApplication.class, args);
+	}
+
+}
